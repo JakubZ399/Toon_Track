db98468d5757b2f71f3cf42e0551cbc242dbdcd5
kubaz <zurawikjakub@gmail.com>
2024-02-17 10:00:44 +0100

    Create BP_Projectile

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -2,12 +2,16 @@
 
 
 #include "Projectile.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	RootComponent = BaseMesh;
 
 }
 
--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -23,4 +23,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UStaticMeshComponent* BaseMesh;
 };
